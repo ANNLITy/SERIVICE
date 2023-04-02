88771ad6f84002c739d7c7614c7d38debf8df881
Al040 <klpk44@yandex.ru>
2023-04-02 21:10:01 +0300

    правки 2

--- src/main/java/Application/Application.java
+++ src/main/java/Application/Application.java
@@ -15,7 +15,18 @@ public class Application {
 
         Role role = new Role(3, "Third_plan");
         User user = new User(3,"Andrey", "T", "CCC", Set.of(role));
-
+        roleDAO.create(role);
+        userDAO.create(user);
+        userDAO.getUsers().forEach(System.out::println);
+        for (User userDAOUser : userDAO.getUsers()) {
+            System.out.println(userDAOUser);
+        }
+            System.out.println(userDAO.readById(1));
+        userDAO.create(user);
+        roleDAO.create(role);
+        userDAO.update(user);
+        userDAO.deleteUser(user);
+        userDAO.getByRole(role);
         roleDAO.getAll().forEach(System.out::println);
         userDAO.getUsers().forEach(System.out::println);
         userDAO.getByRole(role).forEach(System.out::println);
